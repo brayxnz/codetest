4d4c5d429d27f4fd53d4bf3b08f4ccdad2ba888a
brayxnz <brayxncbtis@gmail.com>
2026-05-02 20:38:14 -0700

    fix: corregir variables de entorno

--- CBD.js
+++ CBD.js
@@ -1,9 +1,7 @@
 import { Platform } from 'react-native';
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
 // Solo importa AsyncStorage en móvil
 const getStorage = () => {
   if (Platform.OS === 'web') return undefined; // web usa localStorage automáticamente
@@ -19,3 +17,5 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     detectSessionInUrl: Platform.OS === 'web', // necesario en web para OAuth
   },
 });
+console.log('URL:', process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log('KEY:', process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
