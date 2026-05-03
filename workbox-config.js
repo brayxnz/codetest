34ee1d118862873a3a187e24cbbaa0e53d62cb8f
brayxnz <brayxncbtis@gmail.com>
2026-05-02 18:07:04 -0700

    fix: favicon, manifest y sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   globDirectory: 'dist/',
-  globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+  globPatterns: ['**/*.{js,css,html,png,jpg,svg,gif}'],
   swDest: 'dist/sw.js',
   runtimeCaching: [{
     urlPattern: /^https:\/\/.*\.supabase\.co\/.*/,
-    handler: 'NetworkFirst', // Supabase siempre intenta red primero
+    handler: 'NetworkFirst',
   }]
 }
